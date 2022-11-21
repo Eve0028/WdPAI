798f8b6b47d157f49c4d5bf6a308d755a4306c44
Eve0028 <sylwia14082@gmail.com>
2022-11-21 19:09:59 +0100

    Add routing

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('', 'DefaultController');
+Routing::get('index', 'DefaultController');
+Routing::get('login', 'DefaultController');
+Routing::get('signup', 'DefaultController');
+
+Routing::run($path);
